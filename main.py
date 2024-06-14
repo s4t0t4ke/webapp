6982a886c7f49117bcd08af68a79f4406aa00a47
atfragile <ee207099@meiji.ac.jp>
2024-06-14 17:51:59 +0900

    improve talk

--- main.py
+++ main.py
@@ -5,29 +5,28 @@ import random
 import google.generativeai as genai
 genai.configure(api_key=st.secrets["GEMINI_KEY"])
 
+
 def prepare():
     random.shuffle(data.icon_list)
     random.shuffle(data.role_list)
 
 
 def make_chat(history, idx):
-    model = genai.GenerativeModel('gemini-pro', safety_settings=None)
+    model = genai.GenerativeModel('gemini-pro', safety_settings=data.safety_settings)
     talk_history = ""
     for talk in history:
         user = talk.get("role")
         content = talk.get("content")
         if user == "ai":
             continue
-        #talk_history += f"{data.order[data.icon_list.index(user)]}: {content}\n"
-        talk_history += f"{user}: {content}\n"
+        talk_history += f"Agent[0{user}]: {content}\n"
     talk_history += f"{data.icon_list[idx]}: XXX"
-    prompt = data.PROMPT.format(talk_history)
-    print(prompt)
+    prompt = data.PROMPT.format(idx, talk_history)
     response = model.generate_content(prompt)
     result = response.text
     result = result.replace(" ", "")
-    if result[1] == ":":
-        result = result[2:]
+    if result[9] == ":":
+        result = result[10:]
     return result
 
 
@@ -38,7 +37,8 @@ def main():
     # 最初の起動時だけ実行
     if "history" not in st.session_state:
         prepare()
-        first = data.FIRST.format(data.role_list[0], "")
+        first = data.FIRST.format(data.icon_list[0], data.role_list[0])
+        data.talk_count = 0
         st.session_state["history"] = [{"role": "ai", "content": first}]
 
     # アプリの再実行の際に履歴のチャットメッセージを表示
@@ -48,7 +48,7 @@ def main():
 
     # ユーザー入力に対する反応
     if user_input := st.chat_input("入力", max_chars=100):
-        if data.talk_count < 4:
+        if data.talk_count < 3:
             data.talk_count += 1
             # チャットメッセージコンテナにユーザーメッセージを表示
             icon = data.icon_list[0]
@@ -58,17 +58,16 @@ def main():
             
             for idx in range(1,5):
                 icon = data.icon_list[idx]
-                with st.spinner('思考中...'):
-                    response = make_chat(st.session_state["history"], idx)
+                response = make_chat(st.session_state["history"], idx)
                 # チャットメッセージコンテナにアシスタントのレスポンスを表示
                 st.chat_message(icon).markdown(response)
                 # チャット履歴にアシスタントのレスポンスを追加
                 st.session_state["history"].append({"role": icon, "content": response})
             if data.talk_count == 3:
-                end = "owari"
+                end = "OWARI"
                 st.chat_message("ai").markdown(end)
                 st.session_state["history"].append({"role": "ai", "content": end})
-                # 投票先のボタン
+                # 投票先のボタン表示
 
 
 if __name__ == "__main__":
