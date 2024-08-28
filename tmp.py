124e579c3c8c099d41035c5bf0b4328ef6237534
atfragile <ee207099@meiji.ac.jp>
2024-08-29 03:07:23 +0900

    Add questionnaire

--- tmp.py
+++ tmp.py
@@ -1,38 +1,76 @@
 import streamlit as st
 
-# タブの選択
-#st.header("モード変更")
-tab = st.radio(
-    "",
-    ("タブ1", "タブ2", "タブ3"),
-    label_visibility="collapsed",
-    horizontal=True
-)
+def seikika(n):
+    if n < 10:
+        return 0
+    elif n < 30:
+        return 20
+    elif n < 50:
+        return 40
+    elif n < 70:
+        return 60
+    elif n < 90:
+        return 80
+    elif n < 110:
+        return 100
+    else:
+        return 120
 
-# サイドバーの内容をタブに応じて変更
+def render(n2):
+    n1 = [0, 0, 0, 0, 0]
+    XXX = ""
+    table = {
+        0: "",
+        20: "",
+        40: "",
+        60: "",
+        80: "",
+        100: "",
+        120: ""
+    }
+    m = [table[k] for k in n2]
+    XXX.format(m)
+    n3 = []
+
+# 選択肢のリスト
+choice = [1, 2, 3, 4, 5]
+
+# 2列に分けてラジオボタンを配置
+col1, col2 = st.columns(2)
+
+with col1:
+    st.write("a")
+    answer1 = st.radio("a", choice, label_visibility="collapsed", horizontal=True)
+    st.write("a")
+    answer2 = st.radio("b", choice, label_visibility="collapsed", horizontal=True)
+    st.write("a")
+    answer3 = st.radio("c", choice, label_visibility="collapsed", horizontal=True)
+    st.write("a")
+    answer4 = st.radio("d", choice, label_visibility="collapsed", horizontal=True)
+    st.write("a")
+    answer5 = st.radio("e", choice, label_visibility="collapsed", horizontal=True)
+
+with col2:
+    st.write("a")
+    answer6 = st.radio("f", choice, label_visibility="collapsed", horizontal=True)
+    st.write("a")
+    answer7 = st.radio("g", choice, label_visibility="collapsed", horizontal=True)
+    st.write("a")
+    answer8 = st.radio("h", choice, label_visibility="collapsed", horizontal=True)
+    st.write("a")
+    answer9 = st.radio("i", choice, label_visibility="collapsed", horizontal=True)
+    st.write("a")
+    answer0 = st.radio("j", choice, label_visibility="collapsed", horizontal=True)
+
+# サイドバーに書き込み
 with st.sidebar:
-    st.header(f"{tab}")
-    if tab == "タブ1":
-        st.write("これはタブ1用のサイドバーです。")
-        slider_value = st.slider("スライダー", 0, 100, 50)
-        st.write(f"スライダーの値：{slider_value}")
-    elif tab == "タブ2":
-        st.write("これはタブ2用のサイドバーです。")
-        select_value = st.selectbox("選択ボックス", ["オプションA", "オプションB", "オプションC"])
-        st.write(f"選択された値：{select_value}")
-    elif tab == "タブ3":
-        st.write("これはタブ3用のサイドバーです。")
-        a = st.text_area('Please input any strings', height=500)
-        if a:
-            st.write(a)
-
-# メインコンテンツの表示
-if tab == "タブ1":
-    st.subheader("これはタブ1の内容です。")
-    st.write("ここにタブ1の詳細な情報を表示します。")
-elif tab == "タブ2":
-    st.subheader("これはタブ2の内容です。")
-    st.write("ここにタブ2の詳細な情報を表示します。")
-elif tab == "タブ3":
-    st.subheader("これはタブ3の内容です。")
-    st.write("ここにタブ3の詳細な情報を表示します。")
+    st.write("aaa")
+    if name := st.text_input("bbb",label_visibility="collapsed"):
+        n1 = seikika((answer1 + answer6 - 2) * 10) 
+        n2 = answer2 + answer7
+        n3 = answer3 + answer8
+        n4 = answer4 + answer9
+        n5 = answer5 + answer0
+        n = [n1, n2, n3, n4, n5]
+    #st.button("create")
+    #st.image('image_file_name.jpg')
